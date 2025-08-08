2dccfbf4beed3affe5f97bf7b0f409001a4af812
artpil <artpil@mail.ru>
2025-08-08 10:28:01 +0400

    Add script

---
+++ generate-images-json.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = './images';
+const output = './images/images.json';
+
+const files = fs
+  .readdirSync(dir)
+  .filter(file => /\.(png|jpe?g|gif|webp|svg)$/i.test(file));
+
+fs.writeFileSync(output, JSON.stringify(files, null, 2));
+console.log('✅ images.json created with', files.length, 'files');
