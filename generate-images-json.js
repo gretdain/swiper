59fb2bff7113bd4b4a531a23b7835e7f0228f4a1
artpil <artpil@mail.ru>
2025-08-08 10:43:06 +0400

    Add img

--- generate-images-json.js
+++ generate-images-json.js
@@ -6,7 +6,9 @@ const output = './images/images.json';
 
 const files = fs
   .readdirSync(dir)
-  .filter(file => /\.(png|jpe?g|gif|webp|svg)$/i.test(file));
+  .filter(file => /\.(png|jpe?g|gif|webp|svg)$/i.test(file)) && file !== 'images.json'
+)
+;
 
 fs.writeFileSync(output, JSON.stringify(files, null, 2));
 console.log('✅ images.json created with', files.length, 'files');
